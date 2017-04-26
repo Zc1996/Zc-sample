d046559c9072c3589e06c0fcb52827702a1693fb
Zc <zengcheng1996@126.com>
2017-04-26 17:50:42 +0800

    1th

---
+++ Sample/app/src/main/java/com/microsoft/projectoxford/emotionsample/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.microsoft.projectoxford.emotionsample;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by Zc on 2017/4/26.
+ */
+
+public class LoginActivity extends ActionBarActivity {
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private EditText accountEdit;
+    private EditText passwordEdit;
+    private Button login;
+    private CheckBox remeberPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        accountEdit =(EditText) findViewById(R.id.account);
+        passwordEdit=(EditText)findViewById(R.id.password);
+        remeberPass = (CheckBox)findViewById(R.id.remeber_pass);
+        login = (Button) findViewById(R.id.login);
+        boolean  isRemeber =pref.getBoolean("remeber_password",false);
+        if (isRemeber){
+            //将账号和密码都设置在文本框中
+            String account = pref.getString("account","");
+            String password =pref.getString("password","");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            remeberPass.setChecked(true);
+        }
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String account = accountEdit.getText().toString();
+                String password = passwordEdit.getText().toString();
+                //如果账号是admin且密码是123456，就认为登录成功
+                if (account.equals("admin") && password.equals("123456")) {
+                    editor = pref.edit();
+                    if (remeberPass.isChecked()) {    //检查复选框是否被选中
+                        editor.putBoolean("remeber_password", true);
+                        editor.putString("account", account);
+                        editor.putString("password", password);
+                    } else {
+                        editor.clear();
+                    }
+                    editor.apply();
+                    Intent intent = new Intent(LoginActivity.this, RecognizeActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "account or password is invalid",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+}
